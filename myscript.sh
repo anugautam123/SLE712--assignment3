99ec3b5fc0eb8be24782f89fe572bfd3e3f8bbb5
markziemann <mark.ziemann@gmail.com>
2019-05-23 14:01:03 +1000

    changes in script

--- myscript.sh
+++ myscript.sh
@@ -1,6 +1,8 @@
-formatdb -p F -o T _1 Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
+#
+formatdb -p F -o T -i Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa
 
-msbar _sequence mysample.fa -count 100 -point 4 -block o -codon 0 -outseq mysample_mutated.fa
+#
+msbar -sequence 13.fa -count 700 -point 4 -block 0 -codon 0 -outseq mysample_mutated.fa
 
 blast2 -m 8 -p blastn -e 0.001 -d Escherichia_coli_str_k_12_substr_mg1655.ASM584v2.cds.all.fa -i mysample_mutated.fa
 
